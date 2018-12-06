606d5f3d55468be85a6f4fdbebf5f37e1b247993
Jonathan Vusich <apianist16@gmail.com>
2018-12-06 13:10:57 -0600

    Updated id_generator so that it uses id_generator_interface

--- p2python/crypto/id_generator.py
+++ p2python/crypto/id_generator.py
@@ -1,4 +1,5 @@
 from .node_id import NodeID
+from ..interfaces.id_generator.id_generator_interface import IDGeneratorInterface
 from .utils import verify_public_key, remove_0x_prefix, add_0x_prefix
 
 from hashlib import shake_256
@@ -8,7 +9,7 @@ logger = logging.getLogger("p2python.crypto.id_generator")
 
 """ IDGenerator:
 
-This class is a configurable ID generator for the p2python stack. 
+This class is the base ID generator for the p2python stack. 
 
 
 Functions:  add_public_key
@@ -18,14 +19,8 @@ Functions:  add_public_key
 """
 
 
-class IDGenerator:
-    hash_gen = shake_256()
-    nonce = 0
-
-    def __init__(self):
-        self._public_key = None
-        self._ip_address = None
-        self._port = None
+class IDGenerator(IDGeneratorInterface):
+    hash_generator = shake_256()
 
     def add_public_key(self, public_key: str):
         if verify_public_key(public_key):
@@ -45,3 +40,6 @@ class IDGenerator:
             logger.error("Port is not of type 'int'!")
             raise ValueError
         self._port = str(port).encode()
+
+    def generate_id(self):
+        pass
