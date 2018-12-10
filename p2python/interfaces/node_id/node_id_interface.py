150aecea3f23d7a958be1d9185e0c1ff70e3d8aa
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-12-09 21:24:22 -0600

    Updated interface for ID Generator and NodeID (#10)
    * NodeID now implements NodeIDInterface
    
    * Added node id interface
    
    * Ensured PEP8 compliance in node_id.py
    
    * Added id_generator_interface
    
    * Updated id_generator so that it uses id_generator_interface
    
    * Updated the IDGeneratorInterface class
    
    Now uses properties to avoid accidentally modifying state variables.
    
    Added new ready(self) method that allows the user to check and see if an ID can be generated.
    
    * Changed the timestamp field to nonce instead
    
    This is because timestamps don't actually help improve security, and the nonce field allows any node to quickly verify the hash digest.
    
    * Updated the tests for node_id_interface to reflect the changes made

--- p2python/interfaces/node_id/node_id_interface.py
+++ p2python/interfaces/node_id/node_id_interface.py
@@ -7,10 +7,11 @@ This is the interface for each node ID in the network.
 
 For basic node id classes, NodeIDInterface should simply
 be subclassed as follows:
-    
+
     class NodeID(NodeIDInterface):
         pass
-        
+
+
 Extra helper methods can be added to your NodeID class
 by substituting a custom NodeID class for the base NodeID class.
 """
@@ -21,5 +22,5 @@ class NodeIDInterface(ABC):
     public_key: str
     ip_address: str
     port: int
-    timestamp: int
-    id: str
+    nonce: int
+    id: bytes
