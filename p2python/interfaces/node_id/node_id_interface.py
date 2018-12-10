dea21ecf73577d96b93982ead43993527c46aa83
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-12-09 21:18:33 -0600

    Changed the timestamp field to nonce instead
    This is because timestamps don't actually help improve security, and the nonce field allows any node to quickly verify the hash digest.

--- p2python/interfaces/node_id/node_id_interface.py
+++ p2python/interfaces/node_id/node_id_interface.py
@@ -7,10 +7,10 @@ This is the interface for each node ID in the network.
 
 For basic node id classes, NodeIDInterface should simply
 be subclassed as follows:
-    
+
     class NodeID(NodeIDInterface):
         pass
-        
+
 Extra helper methods can be added to your NodeID class
 by substituting a custom NodeID class for the base NodeID class.
 """
@@ -21,5 +21,5 @@ class NodeIDInterface(ABC):
     public_key: str
     ip_address: str
     port: int
-    timestamp: int
-    id: str
+    nonce: int
+    id: bytes
