0453b363e43a205a044a01703c99b5ed64df66d5
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-12-11 21:15:27 -0600

    Refactored node id tests

--- p2python/tests/crypto/test_node_id.py
+++ p2python/tests/crypto/test_node_id.py
@@ -3,7 +3,7 @@ from dataclasses import FrozenInstanceError
 from p2python.crypto.node_id import NodeID
 
 
-def test_node_id_interface_well_formed():
+def test_node_id_well_formed():
     node = NodeID("string", "string", 123, 123, "string")
     assert node.public_key == "string"
     assert node.ip_address == "string"
@@ -12,7 +12,7 @@ def test_node_id_interface_well_formed():
     assert node.id == "string"
 
 
-def test_node_id_interface_frozen():
+def test_node_id_frozen():
     node = NodeID("string", "string", 123, 123, "string")
     with pytest.raises(FrozenInstanceError):
         node.public_key = "changed"
@@ -26,12 +26,11 @@ def test_node_id_interface_frozen():
         node.id = "changed"
 
 
-def test_node_id_interface_additional_parameters():
+def test_node_id_additional_parameters():
     with pytest.raises(TypeError):
         node = NodeID("string", "string", 123, 123, "string", "string")
 
 
-def test_node_id_interface_lacking_parameters():
+def test_node_id_lacking_parameters():
     with pytest.raises(TypeError):
         node = NodeID("string", "string", 123, 123)
-
