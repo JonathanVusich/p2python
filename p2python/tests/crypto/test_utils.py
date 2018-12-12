c929d0fb8539c601c88750c4a89e3d4f5d30907c
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-12-11 22:39:09 -0600

    Updated tests

--- p2python/tests/crypto/test_utils.py
+++ p2python/tests/crypto/test_utils.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import Mock
 from p2python.crypto.utils import *
 
 
@@ -116,23 +117,14 @@ def test_validate_id_digest_well_formed():
 
 
 def test_generate_id_well_formed():
-    public_key = "0xc0fffe254729295a45a2885639AC7E10F9d5497945a3875639AC7E10F9d54989"
+    public_key = "0xc0fffe254729295a45a2885639AC7E10F8d5497945a3875639AC7E10F9d54989"
     ip_address = "104.218.67.207"
     port = 65536
     node_id = generate_id(public_key, ip_address, port)
-    assert node_id.public_key == "0xc0fffe254729295a45a2885639AC7E10F9d5497945a3875639AC7E10F9d54989"
+    assert node_id.public_key == "0xc0fffe254729295a45a2885639AC7E10F8d5497945a3875639AC7E10F9d54989"
     assert node_id.ip_address == "104.218.67.207"
     assert node_id.port == 65536
-    assert node_id.nonce == 682425
-    assert node_id.id == b'\x00\x00\x01\x93\x96\xd8\t@\xe4\x9f\x9eC\xfc\r\x08\x16L)\x95\x99\xe3\xcb\x1c\xa0\xaf\xdcc' \
-                         b'\xde\xff\x9ftq6\xcao\x94\x01\xd1\xfd\xe1YT\x83\xd9$\xfb\x96]\xa5uq\xc6\xd2\xd1\xd2\x8d=' \
-                         b'\x04\x05-v\x05$\xc5\xfd\xf8pp\x1c\xb8\xb5i\xed\x1d?\x94\x0fHNVO\xffv\x0clC\x81\x1d\xdb\xd9' \
-                         b'\x91\xbc\x90\x0e\n2o\x977\x00\x81\xdf\xac\xb1\xa6\xd1<\x0b\xdaMu\xb4o@\x80\xc2\xc3\xb7\x96' \
-                         b'\x8e\xafx\xfc\x0c\xb2\x84\xae\xe7\x99U\xa3\x9c\x10\x7f\xe3\xc2\xbc\xfd\xaf\nJ\xa5\x1e\xd9' \
-                         b'TEd\xf2\x06\x06pN\xae}\xe0\xaa\xdf\xd5\xe1N|~s</%\xdfQ\xeb\xa6\xec]\xf4\x7f*\xde\xaf\x8a' \
-                         b'\x9b\xae\xf2\xb0\x8be\xae\x00\x9a"%\x81\x99?\xf6\xb0\x89\x92\x85\xea\xa3f\xf2\x82T\xa9\xd8' \
-                         b'\x8eC\xbc\xb1.?\xfe?,\x9c3\xbc\xf0\x8e\x85E6\xce\xe8\xe2\xa9-\xd4\x14\xa8#\x9e\xad\xc5*' \
-                         b'\x881\x8c*\xcc\xe9H\xbd}\x92\xec\xe4\xacl\xd38\xf1\xafu_B'
+    assert node_id.id == "0000013320276ec11fb237e6c3bed22be1f0a08e5c0a1a9b135a803a6835da35"
 
 
 def test_generate_id_bad_public_key():
@@ -159,5 +151,11 @@ def test_generate_id_bad_port():
         node_id = generate_id(public_key, ip_address, port)
 
 
-
-
+def test_validate_id_well_formed():
+    mock_id = Mock()
+    mock_id.public_key = "0xc0fffe254729295a45a2885639AC7E10F9d5497945a3875639AC7E10F9d54989"
+    mock_id.ip_address = "104.218.67.207"
+    mock_id.port = 65536
+    mock_id.nonce = 3476617
+    mock_id.id = "0000013320276ec11fb237e6c3bed22be1f0a08e5c0a1a9b135a803a6835da35"
+    assert validate_id(mock_id)
